4a4ca8fa2244792f90e9dae719a50a03cccec6dc
nozomi <47496184+hnozomi@users.noreply.github.com>
2023-01-11 14:30:15 +0900

    認証実装修正

--- src/components/page/login/Login.page.tsx
+++ src/components/page/login/Login.page.tsx
@@ -1,41 +1,12 @@
-import { NextPage } from 'next';
 import { memo } from 'react';
 
 import { LoginContent } from '@/components/page/login/components/LoginContent';
 import { useLoginPageHook } from '@/components/page/login/hooks/useLoginPageHook';
 
-const LoginPage: NextPage<{ email: string }> = memo(({ email }) => {
-  console.log(email);
+const LoginPage = memo(() => {
   return <LoginContent {...useLoginPageHook()} />;
 });
 
 LoginPage.displayName = Object.keys({ LoginPage }).join();
-
-// export const getServerSideProps: GetServerSideProps = async (ctx) => {
-//   const cookies = nookies.get(ctx);
-//   const session = cookies.session || '';
-
-//   // セッションIDを検証して、認証情報を取得する
-//   const user = await firebaseAdmin
-//     .auth()
-//     .verifySessionCookie(session, true)
-//     .catch(() => null);
-
-//   // 認証情報が無い場合は、ログイン画面へ遷移させる
-//   if (!user) {
-//     return {
-//       redirect: {
-//         destination: '/login',
-//         permanent: false,
-//       },
-//     };
-//   }
-
-//   return {
-//     props: {
-//       email: user.email,
-//     },
-//   };
-// };
 
 export default LoginPage;
