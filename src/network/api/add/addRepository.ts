466695d4be92b784de93e337750b341dac4fdb27
nozomi <47496184+hnozomi@users.noreply.github.com>
2023-01-11 21:47:53 +0900

    不要なファイル削除

--- src/network/api/add/addRepository.ts
+++
@@ -1,7 +0,0 @@
-import { useHttpMethod } from '@/network/useHttpMethod';
-
-export const AddRepository = (endPoint?: string) => {
-  const endpoint = '/Notificaion-of-newPublication_add' + `${endPoint ?? ''}`;
-  const { post } = useHttpMethod(endpoint);
-  return { post };
-};
--- src/network/api/delete/deleteRepository.ts
+++
@@ -1,8 +0,0 @@
-import { useHttpMethod } from '@/network/useHttpMethod';
-
-export const DeleteRepository = (endPoint?: string) => {
-  const endpoint =
-    '/Notificaion-of-newPublication_delete' + `${endPoint ?? ''}`;
-  const { _delete } = useHttpMethod(endpoint);
-  return { _delete };
-};
